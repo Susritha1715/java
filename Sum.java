062b365e0b61cfbe80be0634aa5db378f5723bd4
Susritha1715 <91303111+Susritha1715@users.noreply.github.com>
2021-12-02 10:49:18 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author STUDENT
+ */
+public class Sum {
+     public static void main(String args[])
+    {
+        int num1=10,num2=20,sum;
+        sum = num1+num2;
+        {
+            System.out.println("sum of two numbers: "+sum);
+        }
+    }
+    
+}
+    
+
